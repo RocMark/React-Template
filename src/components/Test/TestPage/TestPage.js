dc6071a03fc47cde18378c34964b481f7bb0fc4e
RocMark <onigir77799@gmail.com>
2020-05-18 16:47:27 +0800

    :zap: ADD renderRoutes on TestPage (not working)

--- src/components/Test/TestPage/TestPage.js
+++ src/components/Test/TestPage/TestPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { renderRoutes } from 'react-router-config'
 
-const TestPage = () => {
+const TestPage = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { route } = props
+
   return (
     <div className='test-page' data-testid='test_page'>
+      {JSON.stringify(route)}
       <h1>TestPage</h1>
+      {renderRoutes(route.routes, { extraProps: { name: '123' } })}
     </div>
   )
 }
