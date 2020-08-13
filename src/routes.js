cb0919357b97fe5b7252fb60c5d7d27c12d0b2b1
RocMark <onigir77799@gmail.com>
2020-08-13 10:28:52 +0800

    :fire: Remove Unused Code

--- src/routes.js
+++ src/routes.js
@@ -3,22 +3,13 @@
 /* Test */
 import ExamplePage from './components/Test/ExamplePage/ExamplePage'
 import TestPage from './components/Test/TestPage/TestPage'
-import TestChildPage from './components/Test/TestChildPage/TestChildPage'
 
 const routes = [
   {
     path: '/test',
     exact: true,
     component: TestPage,
-    breadcrumbName: 'TestPage',
-    routes: [
-      {
-        path: '/test/:id',
-        exact: true,
-        component: TestChildPage,
-        breadcrumbName: 'TestChildPage'
-      }
-    ]
+    breadcrumbName: 'TestPage'
   },
   {
     path: '*',
